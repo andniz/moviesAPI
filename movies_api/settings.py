8537d30e10623e11003f62d873ea63c7e5c54ff3
Niziolek Andrzej <andrzej.niziolek@nokia.com>
2019-05-25 18:56:13 +0200

    set append_slash to False

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+APPEND_SLASH = False
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 django_heroku.settings(locals())
